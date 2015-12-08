a9088473951fce1886d45f4fd3e57cb1665a1533
CarlNathan <c.n.udren@gmail.com>
2015-12-07 19:53:04 -0500

    Wrote FizzBuzz Program in MainC file.  Compiled the program and added resulting xcode files. i Please enter the commit message for your changes. Lines starting

--- C-Starter-Project/main.c
+++ C-Starter-Project/main.c
@@ -8,8 +8,31 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int x = 0;
+    
+    for (x = 1; x <= 100; x++)
+    {
+    
+    if (x % 3 != 0 && x % 5 != 0)
+    {
+        printf("%d", x);
+    }
+    
+        if (x % 3 == 0)
+    {
+        printf("Fizz");
+    }
+        
+    if (x % 5 == 0)
+    {
+        printf("Buzz");
+    }
+    
+    printf("\n");
+    }
+    
+    
     return 0;
 }
